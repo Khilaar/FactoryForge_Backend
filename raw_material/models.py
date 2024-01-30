82c2199d54ad73a2eda7108af9601e6e7e8f7611
janishiestand <janis.hiestand@gmail.com>
2024-01-30 23:23:17 +0100

    Product implemented. Requires raw_material implementation to complete and test.

--- raw_material/models.py
+++ raw_material/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class RawMaterial(models.Model):
-    title = models.CharField(max_length=250)
+    name = models.CharField(max_length=250)
     quantity = models.IntegerField()
     restock_required = models.BooleanField(default=False)
     max_quantity = models.IntegerField()
