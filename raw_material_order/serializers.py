7495d403ec5802ca4b6b2b28c436d542e3340d35
janishiestand <janis.hiestand@gmail.com>
2024-02-02 19:57:53 +0100

    RawMaterialOrder supplier representation added.

--- raw_material_order/serializers.py
+++ raw_material_order/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
 
+from custom_user.models import CustomUser
 from raw_material_order.models import RawMaterialOrder
+
+
+class CustomSupplierSerializer(serializers):
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'first_name', 'last_name', 'username', 'type_of_user')
 
 
 class RawMaterialOrderSerializer(serializers.ModelSerializer):
@@ -14,3 +21,8 @@ class RawMaterialOrderSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         pass
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['supplier'] = CustomSupplierSerializer(instance.supplier).data
+        return representation
