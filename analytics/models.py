4abbc8bcfba25cd025b2c18ee5d29932b3017543
Khilaar <luca.oliverio@hotmail.com>
2024-02-04 00:17:54 +0100

    added relation between client_order & analytics and raw_material_order & analytics. Adjusted the AnalyticsSerializer

--- analytics/models.py
+++ analytics/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db.models import OneToOneField
 
 
 # Create your models here.
--- analytics/serializers.py
+++ analytics/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 
 from analytics.models import Analytics
+from client_order.serializers import ClientOrderSerializer
 
 
 class AnalyticsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Analytics
-        fields = ['name', 'date', 'description', 'user', 'inventory', 'product_inventory']
+        fields = ['name', 'date', 'description', 'user', 'inventory', 'product_inventory', 'client_orders']
