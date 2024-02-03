1cca332b398bcd43fac3c28a726a25dd42969145
Khilaar <luca.oliverio@hotmail.com>
2024-02-03 18:53:48 +0100

    Corrected last commit

--- factoryforge/urls.py
+++ factoryforge/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('api/products/', include('product.urls')),
     path('api/inventory/', include('inventory.urls')),
     path('api/productinventory/', include('product_inventory.urls')),
+    path('api/users/', include('custom_user.urls')),
     path('api/client_orders/', include('client_order.urls')),
     path('api/suppliers/', include('supplier.urls')),
     path('api/raw_materials_orders/', include('raw_material_order.urls')),
