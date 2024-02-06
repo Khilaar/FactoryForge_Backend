35656db6255a7db9d86bda9ea45b1ae7751c1872
janishiestand <janis.hiestand@gmail.com>
2024-02-07 00:09:26 +0100

    UsedRawMaterialView implemented.

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from analytics.views import ListCreateAnalyticsView, ProfitView
+from analytics.views import ListCreateAnalyticsView, ProfitView, SoldProductsView, UsedRawMaterialView
 
 urlpatterns = [
     path('', ListCreateAnalyticsView.as_view()),
-    path('profit/', ProfitView.as_view())
+    path('profit/', ProfitView.as_view()),
+    path('soldproducts/', SoldProductsView.as_view()),
+    path('usedrawmaterials/', UsedRawMaterialView.as_view())
 ]
