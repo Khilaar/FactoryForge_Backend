2fea1ddb5e8b8bf748130349bc0309574c63867a
Khilaar <luca.oliverio@hotmail.com>
2024-02-07 19:58:56 +0100

    Saw a small change I did for testing purposes in analytics model, default value. Changed it back to None

--- analytics/models.py
+++ analytics/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Analytics(models.Model):
-    name = models.CharField(max_length=200, default="Analytics")
+    name = models.CharField(max_length=200, default=None)
     description = models.TextField(blank=True, null=True)
     user = models.ForeignKey('custom_user.CustomUser', on_delete=models.PROTECT)
     inventory = models.ForeignKey('inventory.Inventory', on_delete=models.PROTECT)
